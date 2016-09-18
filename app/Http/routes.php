b5c45700b3634ee24e9611536498958e00344fbb
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2016-09-18 18:43:12 +0530

    routes updated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,115 +13,116 @@
 
 //Route::group(['middleware' => ['web']], function () {
 
-    Route::get('/push', 'TestController@push');
+Route::get('/push', 'TestController@push');
 
-    Route::get('/test', function () {
-        return View::make('test');
-    });
+Route::get('/test', function () {
+    return View::make('test');
+});
 
-    Route::get('/', function () {
-        if (Auth::check()) {
-            if (Auth::user()->userLevel->id == 4) {
-                // Normal user
-                return view('customer.mainlayout');
-            } else {
-                return redirect("/dashboard");
-            }
-        } else {
+Route::get('/', function () {
+    if (Auth::check()) {
+        if (Auth::user()->userLevel->id == 4) {
+            // Normal user
             return view('customer.mainlayout');
-        }
-    });
-
-    Route::get('/login', function () {
-        if (Auth::check()) {
+        } else {
             return redirect("/dashboard");
-        } else {
-            return View::make('login');
         }
-    });
+    } else {
+        return view('customer.mainlayout');
+    }
+});
 
-    Route::get('/dashboard', function () {
-        if (Auth::check()) {
-            return View::make('admin.dashboard');
-        } else {
-            return redirect("/login");
-        }
-    });
+Route::get('/login', function () {
+    if (Auth::check()) {
+        return redirect("/dashboard");
+    } else {
+        return View::make('login');
+    }
+});
 
-    Route::get('/newhire', function () {
-        if (Auth::check()) {
-            return View::make('newhire');
-        } else {
-            return redirect("/login");
-        }
-    });
+Route::get('/dashboard', function () {
+    if (Auth::check()) {
+        return View::make('admin.dashboard');
+    } else {
+        return redirect("/login");
+    }
+});
 
-    Route::get('/ongoingorders', function () {
-        if (Auth::check()) {
-            return View::make('ongoing-orders');
-        } else {
-            return redirect("/login");
-        }
-    });
+Route::get('/newhire', function () {
+    if (Auth::check()) {
+        return View::make('newhire');
+    } else {
+        return redirect("/login");
+    }
+});
 
-    Route::get('/orderhistory', function () {
-        if (Auth::check()) {
-            return View::make('orderhistory');
-        } else {
-            return redirect("/login");
-        }
-    });
+Route::get('/ongoingorders', function () {
+    if (Auth::check()) {
+        return View::make('ongoing-orders');
+    } else {
+        return redirect("/login");
+    }
+});
 
-    Route::get('/accounts/view', function () {
-        if (Auth::check()) {
-            return View::make('admin.viewAccounts');
-        } else {
-            return redirect("/login");
-        }
-    });
+Route::get('/orderhistory', function () {
+    if (Auth::check()) {
+        return View::make('orderhistory');
+    } else {
+        return redirect("/login");
+    }
+});
 
-    Route::get('/taxis/edit/{taxi}', function () {
-        if (Auth::check()) {
-            return View::make('edittaxi');
-        } else {
-            return redirect("/login");
-        }
-    });
+Route::get('/accounts/view', function () {
+    if (Auth::check()) {
+        return View::make('admin.viewAccounts');
+    } else {
+        return redirect("/login");
+    }
+});
 
-    Route::post('/login', 'AuthController@loginWeb');
-    Route::get('/logout', 'AuthController@logoutWeb');
+Route::get('/taxis/edit/{taxi}', function () {
+    if (Auth::check()) {
+        return View::make('edittaxi');
+    } else {
+        return redirect("/login");
+    }
+});
 
-    Route::get('/updates', 'WebController@getDriverUpdates');
+Route::post('/login', 'AuthController@loginWeb');
+Route::get('/logout', 'AuthController@logoutWeb');
 
-    Route::get('/neworder', 'OrderController@showNewOrderPage');
-    Route::get('/taxioperator/order/new', 'CustomerController@placeOrderByTaxiOperator');
-    Route::get('/taxioperator/order/state', 'OrderController@getOrderState');
+Route::get('/updates', 'WebController@getDriverUpdates');
 
-    Route::get('/accounts/delete/{id}', 'UserController@deleteUser');
-    Route::get('/accounts/view/{user}', 'UserController@showViewPage');
-    Route::get('/accounts/edit/{user}', 'UserController@showEditPage');
-    Route::post('/accounts/update/{user}', 'UserController@updateUser');
-    Route::get('/accounts/new', 'UserController@showNewUserPage');
-    Route::post('/accounts/new', 'UserController@createNewUser');
+Route::get('/neworder', 'OrderController@showNewOrderPage');
+Route::get('/taxioperator/order/new', 'CustomerController@placeOrderByTaxiOperator');
+Route::get('/taxioperator/order/state', 'OrderController@getOrderState');
 
-    Route::get('/taxis/new', 'TaxiController@showNewTaxiPage');
-    Route::get('/taxis/view', 'TaxiController@showViewTaxisPage');
-    Route::get('/taxis/edit/{taxi}', 'TaxiController@showEditTaxisPage');
-    Route::post('/taxis/update/{taxi}', 'TaxiController@updateTaxi');
-    Route::post('/taxis/new', 'TaxiController@createNewTaxi');
+Route::get('/accounts/delete/{id}', 'UserController@deleteUser');
+Route::get('/accounts/view/{user}', 'UserController@showViewPage');
+Route::get('/accounts/edit/{user}', 'UserController@showEditPage');
+Route::post('/accounts/update/{user}', 'UserController@updateUser');
+Route::get('/accounts/new', 'UserController@showNewUserPage');
+Route::post('/accounts/new', 'UserController@createNewUser');
 
-    Route::get('/ongoing-orders', 'OrderController@showOnGoingOrdersPage');
-    Route::get('/ongoing-orders/get', 'OrderController@getOngoingOrders');
-    Route::get('/finished-orders', 'OrderController@showFinishedOrdersPage');
-    Route::get('/finished-orders/get', 'OrderController@getFinishedOrders');
-    Route::post('/add-reviews', 'ReviewController@rateDriver');
-    Route::get('/view-reviews/{driver_id}', 'ReviewController@getDriverRatings');
-    Route::get('/view-reviews/getAllReviews', 'ReviewController@getAllReviews');
-    // customer routes
-    Route::get('/signup/', function (){
-        return view('signup');
-    });
-    Route::post('/signup/', 'AuthController@signUp');
+Route::get('/taxis/new', 'TaxiController@showNewTaxiPage');
+Route::get('/taxis/view', 'TaxiController@showViewTaxisPage');
+Route::get('/taxis/edit/{taxi}', 'TaxiController@showEditTaxisPage');
+Route::post('/taxis/update/{taxi}', 'TaxiController@updateTaxi');
+Route::post('/taxis/new', 'TaxiController@createNewTaxi');
+
+Route::get('/ongoing-orders', 'OrderController@showOnGoingOrdersPage');
+Route::get('/ongoing-orders/get', 'OrderController@getOngoingOrders');
+Route::get('/finished-orders', 'OrderController@showFinishedOrdersPage');
+Route::get('/finished-orders/get', 'OrderController@getFinishedOrders');
+Route::post('/add-reviews', 'ReviewController@rateDriver');
+Route::get('/view-reviews/{driver_id}', 'ReviewController@getDriverRatings');
+Route::get('/view-reviews/getAllReviews', 'ReviewController@getAllReviews');
+// customer routes
+Route::get('/signup/', function () {
+    return view('signup');
+});
+Route::post('/signup/', 'AuthController@signUp');
+Route::get('/reviews/', 'ReviewController@getReviews');
 //});
 
 Route::group(['middleware' => ['api']], function () {
@@ -150,7 +151,5 @@ Route::group(['middleware' => ['api']], function () {
     Route::get('/newoffer/', 'OffersController@newOfferView');
     Route::post('/newoffer/', 'OffersController@newOfferSave');
     Route::get('/offerhistory/', 'OffersController@offerHistory');
-
-    Route::get('/reviews/', 'ReviewController@getReviews');
 
 });
